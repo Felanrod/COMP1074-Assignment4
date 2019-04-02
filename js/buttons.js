10a17fd07125fe9fa306e9f6bff75e0c5e9d3441
Joel Murphy <200203095@student.georgianc.on.ca>
2019-04-02 00:23:02 -0400

    Added Comments to find where the content is

--- js/buttons.js
+++ js/buttons.js
@@ -5,6 +5,7 @@ const areaBtn = document.querySelector('#areaBtn');
 const campusPathStart = 'https://www.georgiancollege.ca/academics/full-time-programs/?campus=';
 const areaPathStart = 'https://www.georgiancollege.ca/academics/academic-areas/';
 
+// The select elements sets which page to send the user to and the "Find" buttons sends them to it
 campusBtn.addEventListener('click', () => {
     if (campusSelect.value != '') {
         let campusPath = campusPathStart + campusSelect.value;
